19f91a0c0812de748ca980a1d60d38a56da9ff54
Kauan-Code19 <pkauprofissional@gmail.com>
2024-10-22 21:11:32 -0300

    ajuste em enviroment development

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080',
-    brokerURL: 'ws://localhost:8080/realtime'
+    apiUrl: '' , //'http://localhost:8080',//
+    brokerURL: '' //'ws://localhost:8080/realtime'//
 }
 
 
-if (typeof window !== 'undefined') {
-    environment.apiUrl = `http://${window.location.hostname}:8080`
-    environment.brokerURL = `ws://${window.location.hostname}:8080/realtime`
-}
+// if (typeof window !== 'undefined') {
+//     environment.apiUrl = `http://${window.location.hostname}:8080`
+//     environment.brokerURL = `ws://${window.location.hostname}:8080/realtime`
+// }
